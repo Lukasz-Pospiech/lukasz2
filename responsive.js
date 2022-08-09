a63427b307a4cea88df476ebd00571ca33bfacd9
Lukasz-Pospiech <110336845+Lukasz-Pospiech@users.noreply.github.com>
2022-08-09 21:22:04 +0700

    Add files via upload

---
+++ responsive.js
@@ -0,0 +1,32 @@
+
+let menuState = 'closed';
+function menuShowOrHide() {
+    let menuDiv = document.getElementById("menuDIV");
+    let menuDiv2 = document.getElementById("menuDIV2");
+
+    document.getElementById("navBar").style.backgroundColor = "green"; 
+
+    if (menuState === 'closed') {
+        menuShow();
+    }
+    else if (menuState === 'opened') {
+        menuHide();
+    }
+
+    function menuShow() {
+        // document.getElementById("hamburgerIcon").style.display = "none";
+        // document.getElementById("crossIcon").style.display = "inline-block";
+        menuDiv.className = "menuDivHidden menuDivHiddenEject";
+        menuDiv2.className = "menuDivHidden2 menuDivHiddenEject2";
+        menuState = 'opened';
+    }
+    function menuHide() {
+        // document.getElementById("crossIcon").style.display = "none";
+        // document.getElementById("hamburgerIcon").style.display = "inline-block";
+        menuDiv.className = "menuDivShown";
+        menuDiv2.className = "menuDivShown2";
+        menuState = 'closed';
+    }
+}
+
+
